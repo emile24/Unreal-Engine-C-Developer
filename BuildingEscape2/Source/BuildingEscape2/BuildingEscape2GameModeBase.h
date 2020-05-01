b9143d733ed850e316b5e4871bb0e71741857f9e
emile24 <emile.pettersen.coulombe@gmail.com>
2020-05-01 11:09:58 -0400

    Init building escape 2

---
+++ BuildingEscape2/Source/BuildingEscape2/BuildingEscape2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscape2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE2_API ABuildingEscape2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
